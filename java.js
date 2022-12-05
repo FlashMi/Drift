87f769005dd4e38ef1b8fca2ff09ca74600a5f76
FlashMi <119871239+FlashMi@users.noreply.github.com>
2022-12-05 18:10:08 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+function z(){
+    alert("Вы выбрали автомобиль Nissan z370");
+  }
+
+function sil(){
+    alert("Вы выбрали автомобиль Nissan silvia s15");
+}
+
+function sky(){
+    alert("Вы выбрали автомобиль Nissan skiline r34");
+}
+
+function but(){
+    alert("Выберите необходимый автомобиль и нажмите на него");
+}
